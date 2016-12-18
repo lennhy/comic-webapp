0410caa6e6916d62e69eee2c5e01fda32af497b2
lenn hypolite <lennhydesign@gmail.com>
2016-12-17 21:44:09 -0500

    Edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'comics#index'
+  root 'application#index'
   devise_for :users
 
-  resources :comics
+  resources :comics [:index, :show, :new, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
