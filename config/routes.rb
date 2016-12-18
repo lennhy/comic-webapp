ce9e3bc1053700b74bbe525713d75c42b4058e62
lenn hypolite <lennhydesign@gmail.com>
2016-12-17 21:45:50 -0500

    Corrected route syntax error

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'application#index'
   devise_for :users
 
-  resources :comics [:index, :show, :new, :create, :update, :destroy]
+  resources :comics, only: [:index, :show, :new, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
