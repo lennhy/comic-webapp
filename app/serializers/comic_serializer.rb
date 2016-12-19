55af6b0be52929b6524c1be8823fb3ab0319f1dd
lenn hypolite <lennhydesign@gmail.com>
2016-12-19 12:29:28 -0500

    Added user role and relationships to comic serializer

--- app/serializers/comic_serializer.rb
+++ app/serializers/comic_serializer.rb
@@ -1,10 +1,6 @@
 class ComicSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :issue, :volume, :page_count, :issue_date, :graphic_novel
+  attributes :id, :title, :description, :issue, :volume, :page_count, :issue_date, :graphic_novel, :region_id
+  has_many :users, serializer: UserSerializer
+  has_many :genres
+  has_one :region
 end
-# t.string :title
-# t.text :description
-# t.integer :issue
-# t.integer :volume
-# t.integer :page_count
-# t.date :issue_date
-# t.boolean :graphic_novel, :default=> false
