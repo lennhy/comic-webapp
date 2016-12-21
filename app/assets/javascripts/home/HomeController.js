db1735667f78d3e2faec25650f67cda4f16e29e4
lenn hypolite <lennhydesign@gmail.com>
2016-12-21 15:47:23 -0500

    Added more elements to books home page

--- app/assets/javascripts/home/HomeController.js
+++ app/assets/javascripts/home/HomeController.js
@@ -3,7 +3,7 @@ function HomeController(books) {
   vm.books = books.data;
   vm.genres = books.data[0].genres;
   vm.regions = books.data[0].region;
-
+  console.log(books.data)
   // console.log(books.data[0].region);
 
 
@@ -16,3 +16,40 @@ function HomeController(books) {
 angular
         .module('app')
         .controller('HomeController', HomeController);
+
+
+        [
+          {
+            "id": 1,
+            "title": "The Best",
+            "description": "A story about a man and his dreams of becoming the world's best coder",
+            "issue": 1,
+            "volume": 2,
+            "page_count": 22,
+            "issue_date": "2012-12-12",
+            "graphic_novel": false,
+            "region_id": 8,
+            "users": [
+              {
+                "id": 10,
+                "name": "Bill",
+                "email": "bill@gmail.com",
+                "role": "publisher"
+              }
+            ],
+            "genres": [
+              {
+                "id": 10,
+                "name": "Horror",
+                "created_at": "2016-12-18T02:35:29.361Z",
+                "updated_at": "2016-12-18T02:35:29.361Z"
+              }
+            ],
+            "region": {
+              "id": 8,
+              "name": "Africa",
+              "created_at": "2016-12-18T02:35:28.250Z",
+              "updated_at": "2016-12-18T02:35:28.250Z"
+            }
+          }
+        ]
