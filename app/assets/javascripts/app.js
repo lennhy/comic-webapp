a0640a1aab27ac5c5d1fc609765162c937bfa927
lenn hypolite <lennhydesign@gmail.com>
2016-12-23 20:39:22 -0500

    Added a filter to show only the users that are publishers

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,5 +6,13 @@
       .module('app', ['ui.router', 'templates', 'ngMessages'])
       .config(function($httpProvider){
           $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+      })
+      .filter('returnPublisher', function () {
+          return function (user) {
+              if(user.role === "publisher"){
+                return user.name;
+              }
+          }
       });
+
 }());
--- app/assets/javascripts/books/BooksController.js
+++ app/assets/javascripts/books/BooksController.js
@@ -1,29 +1,29 @@
 function BooksController(books) {
   var vm = this;
   vm.books = books.data;
-  vm.userNames;
-  vm.region;
-  vm.genres;
 
-  function genres(genres){
-    genres.forEach(function(val, i){
-
-    })
-   }
-
-   function userNames(){
-      var usernames =[];
-      var region='';
-      var genres=[];
-      var j=0;
-      for(let i=0; i < users.length; i++){
-        if(users[i][j].role === "publisher") {
-          usernames.push(users[i][j].name);
-        }
-      }
-      vm.userNames = (usernames).join(",");
-    }
-    userNames();
+//   function returnPublisher(user){
+//       // for(let i=0; i < users.length; i++){
+//         if (user ==="publisher"){
+//           return user.name;
+//         }
+//       // }
+//   }
+//   vm.filteredList = $filter('filter')(vm.users, returnPublisher(vm.user));
+//
+// //    function userNames(){
+// //       var usernames =[];
+// //       var region='';
+// //       var genres=[];
+// //       var j=0;
+// //       for(let i=0; i < users.length; i++){
+// //         if(users[i][j].role === "publisher") {
+// //           usernames.push(users[i][j].name);
+// //         }
+// //       }
+// //       vm.userNames = (usernames).join(",");
+//     }
+//     userNames();
 }
 
 angular
