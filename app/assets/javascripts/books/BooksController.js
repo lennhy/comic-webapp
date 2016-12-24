7f00b92cf0bd8a761c13292daf384eb1a89934bb
lenn hypolite <lennhydesign@gmail.com>
2016-12-23 21:12:26 -0500

    Added a search filter feature but DOM is blank until search entered

--- app/assets/javascripts/books/BooksController.js
+++ app/assets/javascripts/books/BooksController.js
@@ -1,29 +1,14 @@
-function BooksController(books) {
+function BooksController(books, $filter) {
   var vm = this;
   vm.books = books.data;
+  vm.search = '';
 
-//   function returnPublisher(user){
-//       // for(let i=0; i < users.length; i++){
-//         if (user ==="publisher"){
-//           return user.name;
-//         }
-//       // }
-//   }
-//   vm.filteredList = $filter('filter')(vm.users, returnPublisher(vm.user));
-//
-// //    function userNames(){
-// //       var usernames =[];
-// //       var region='';
-// //       var genres=[];
-// //       var j=0;
-// //       for(let i=0; i < users.length; i++){
-// //         if(users[i][j].role === "publisher") {
-// //           usernames.push(users[i][j].name);
-// //         }
-// //       }
-// //       vm.userNames = (usernames).join(",");
-//     }
-//     userNames();
+  if(vm.search == ''){
+    vm.filteredList = vm.books;
+  }
+  vm.updateSearch = function(){
+    vm.filteredBooks = $filter('filter')(vm.books, vm.search);
+  };
 }
 
 angular
