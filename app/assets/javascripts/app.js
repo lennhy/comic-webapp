3a861e813a6719398a0fbd9a8b46a529da3f62ba
lenn hypolite <lennhydesign@gmail.com>
2016-12-23 20:44:22 -0500

    created a seperate filters file

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,13 +6,7 @@
       .module('app', ['ui.router', 'templates', 'ngMessages'])
       .config(function($httpProvider){
           $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-      })
-      .filter('returnPublisher', function () {
-          return function (user) {
-              if(user.role === "publisher"){
-                return user.name;
-              }
-          }
       });
+
 
 }());
---
+++ app/assets/javascripts/filters.js
@@ -0,0 +1,9 @@
+angular
+  .module('app')
+      .filter('returnPublisher', function () {
+            return function (user) {
+                if(user.role === "publisher"){
+                  return user.name;
+                }
+            }
+        });
