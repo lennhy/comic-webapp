0686944801b10922f108b10bb77a91c900719370
lenn hypolite <lennhydesign@gmail.com>
2017-02-03 15:34:07 -0500

    Added a custom attribute writter for nested attribute

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -12,4 +12,21 @@ class Comic < ApplicationRecord
 
   validates :users, :title, :description, :issue, :volume, :page_count, :issue_date, :region_id,  presence: true
   validates_inclusion_of :graphic_novel, :in => [true, false]
+
+
+  # --nested forms custom attribute writter
+  def pages_attributes=(pages_attributes)
+     pages_attributes.values.each do |pages_attribute|
+       if pages_attribute[:illustration].blank? || pages_attribute[:comic_id].blank?
+
+       else
+         page = Page.find_or_create_by(pages_attribute)
+         self.pages << page
+       end
+     end
+   end
+
+
+
+
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -2,9 +2,9 @@ class Page < ApplicationRecord
   belongs_to :comic
   # validates :pages, presence: true
 
-  has_attached_file :image, :styles => {large: "1000x1000>", medium: "300x300>", thumb: "150x150#" },
+  has_attached_file :illustration, :styles => {large: "1000x1000>", medium: "300x300>", thumb: "150x150#" },
                             :default_style => :thumb, :default_url=> "/images/:style/cover.png"
 
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :illustration, content_type: /\Aimage\/.*\z/
 
 end
