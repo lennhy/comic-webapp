9038dbc4e5e7d8f778707fa5043eba7a2882c9b4
lenn hypolite <lennhydesign@gmail.com>
2017-02-03 22:49:18 -0500

    generated a cover paperclip image for comic model

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -5,9 +5,12 @@ class Comic < ApplicationRecord
   has_many :comic_genres
   has_many :ratings
   has_many :pages
-
   belongs_to :region
 
+  has_attached_file :cover, :styles => {large: "1000x1000>", medium: "300x300>", thumb: "150x150#" },
+                            :default_style => :thumb, :default_url=> "/images/:style/cover.png"
+
+  validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
 
   validates :users, :title, :description, :issue, :volume, :page_count, :issue_date, :region_id,  presence: true
   validates_inclusion_of :graphic_novel, :in => [true, false]
