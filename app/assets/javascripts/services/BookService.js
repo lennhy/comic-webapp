53ea57e3024e9999bf4d11d1248b17a78c5ff461
lenn hypolite <lennhydesign@gmail.com>
2017-02-04 03:12:09 -0500

    Created a fileService for grabing the file with html

--- app/assets/javascripts/services/BookService.js
+++ app/assets/javascripts/services/BookService.js
@@ -1,20 +1,8 @@
 function BookService($http){
 
-  // success and error are functions on a promise that AngularJS adds for
-  // us when using $http or $resource. They're not standard, you won't
-  //  find them on other promises.
-  //
-  // When you are using a promise, the function you should call is then.
-  // then takes two parameters - a callback function for success and a
-  // callback function for failure.
   this.httpGetAllBooks = function(){
     console.log("d");
     return $http.get('/comics.json')
-        // success and error are special functions added to a promise by $http
-
-        // success or error will be called later - when this block is finished
-        // executing we don't have the books data, we've just specified what to do
-        // when we do eventually get it - or what to do if we fail to get it.
   };
 
 
@@ -22,7 +10,7 @@ function BookService($http){
     return $http.get('/comics/'+ id);
 
   };
-//
+
   this.httpCreateBook = function(data) {
     var req = {
      method: 'POST',
@@ -36,25 +24,14 @@ function BookService($http){
     .then(successCallback)
     .catch(errorCallback)
 
-    // returns a promise
-    // What to do when the request succeeds
-    // Success
     function successCallback(data){
       console.log(data)
     }
-    // what to do when the request fails
-    // failure
+
     function errorCallback(error){
       console.log(error)
     }
-}
-  // success and error are special functions added to a promise by $http
-
-  // success or error will be called later - when this block is finished
-  // executing we don't have the name, we've just specified what to do
-  // when we do eventually get it - or what to do if we fail to get it.
-  // Promises are not actually complicated, they're objects that contain a
-  // reference to functions to call when something fails or succeeds.
+  }
 
   this.httpAdd = function(id){
     var req = {
@@ -68,8 +45,6 @@ function BookService($http){
   this.httpUpdateBook = function(id){
     return $http.patch(`/comics/${id}`);
   };
-
-
 
   this.httpDeleteBook = function(id){
     return $http.delete(`/comics/${id}`);
---
+++ app/assets/javascripts/services/FileService.js
@@ -0,0 +1,23 @@
+function FileService($http){
+
+  // preview image before upload
+  this.previewImg = function(div,  displayDiv){
+  $(div).on('change', function(event) {
+     var files = event.target.files;
+     var image = files[0]
+     var reader = new FileReader();
+     reader.onload = function(file) {
+       var img = new Image();
+       console.log(file);
+       img.src = file.target.result;
+       img.height=200;
+      //  img.width = auto;
+       $(displayDiv).html(img);
+     }
+     reader.readAsDataURL(image);
+   });
+  }
+
+angular
+    .module('app')
+    .service('FileService', FileService);
