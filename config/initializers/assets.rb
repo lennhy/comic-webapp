ff1ec3f1bc948be98877b20289947e3638d72a77
lennhy <lennhydesign@gmail.com>
2018-04-02 10:29:06 -0400

    Add assets paths to assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-Rails.application.config.assets.paths.root.join('vendor', 'assets', 'bower_components', 'public')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'public')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
