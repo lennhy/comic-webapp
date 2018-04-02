6817252a0124fd43cf5fd8a3d78b989876e0b2f6
lennhy <lennhydesign@gmail.com>
2018-04-02 14:28:48 -0400

    Replace code in assets.rb with default code

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,17 +1,11 @@
-class Application < Rails::Application
+# Be sure to restart your server when you modify this file.
 
-  config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
 
-end
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
 
-# # Be sure to restart your server when you modify this file.
-#
-# # Version of your assets, change this if you want to expire all your assets.
-# Rails.application.config.assets.version = '1.0'
-#
-# # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Rails.root.join('bower_components')
-#
-# # Precompile additional assets.
-# # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# # Rails.application.config.assets.precompile += %w( search.js )
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
