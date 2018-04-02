79c4c3355560b2fa72195cb02277c47c3893885e
lennhy <lennhydesign@gmail.com>
2018-04-01 22:22:03 -0400

    Comment out carrierwave file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,21 @@
-# if Rails.env.development? or Rails.env.test?
-  # CarrierWave.configure do |config|
-  #   config.storage = :file
-  #   config.enable_processing = false
-  # end
-# else
-  CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'                        # required
-    config.fog_credentials = {
-      provider:              "AWS",                        # required
-      aws_access_key_id:     ENV["AWS_SECRET_ACCESS_ID"],                        # required
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]                      # required
-      # region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
-      # host:                  's3.example.com',             # optional, defaults to nil
-      # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
-    }
-    config.fog_directory  = ENV["AWS_S3_BUCKET"]                                   # required
-    config.fog_public     = false                                                 # optional, defaults to true
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
-  end
-# end
+# # if Rails.env.development? or Rails.env.test?
+#   # CarrierWave.configure do |config|
+#   #   config.storage = :file
+#   #   config.enable_processing = false
+#   # end
+# # else
+#   CarrierWave.configure do |config|
+#     config.fog_provider = 'fog/aws'                        # required
+#     config.fog_credentials = {
+#       provider:              "AWS",                        # required
+#       aws_access_key_id:     ENV["AWS_SECRET_ACCESS_ID"],                        # required
+#       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]                      # required
+#       # region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+#       # host:                  's3.example.com',             # optional, defaults to nil
+#       # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+#     }
+#     config.fog_directory  = ENV["AWS_S3_BUCKET"]                                   # required
+#     config.fog_public     = false                                                 # optional, defaults to true
+#     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+#   end
+# # end
